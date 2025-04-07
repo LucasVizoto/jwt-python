79b888a0899500de32def215ae1dbd82aa6d3f2c
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-04-06 21:57:40 -0300

    feat: implementing user repository with connection (need tests)

--- src/models/repositories/user_repository.py
+++ src/models/repositories/user_repository.py
@@ -13,3 +13,28 @@ class UserRepository:
             ''', (username, password, 0)
         )
         self.__conn.commit() #commitando o meu insert
+
+    def edit_balance(self, user_id:int, new_balance: float) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            UPDATE users 
+            SET balance = ? 
+            WHERE id = ?;
+            ''', (new_balance, user_id)
+        )
+        self.__conn.commit()
+    
+    def get_user_by_username(self, username: str):
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            SELECT id, username, password
+            FROM users 
+            WHERE username = ?;
+            ''', (username,) #eu preciso sempre deixar uma vírgula a mais no select
+        )
+        user = cursor.fetchone()
+        return user 
+    # como é um select, eu nn faço commit
+    # fetch one busca um, mas com many, você consegue buscar vários
--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,12 +1,16 @@
 from .user_repository import UserRepository
 from src.models.settings.db_connectio_handler import db_connection_handler
 
-def test_registry_user():
+def test_repository():
     db_connection_handler.connect()
     conn = db_connection_handler.get_connection()
     repo = UserRepository(conn)
 
-    username = "Lucas"
+    username = "Pedro"
     password = "1234"
 
     repo.registry_user(username, password)
+    user = repo.get_user_by_username(username)
+    print(f'\n\n {user}') #retornando uma tupla
+
+    # sendo assim eu posso usar o indice pra apenas pegas a informação que eu quero
