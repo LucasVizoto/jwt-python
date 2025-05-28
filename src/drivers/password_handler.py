25c27f0dd753fece043eac5ec292a4878a0a072d
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-05-28 18:55:37 -0300

    feat: implementing with routes and controllers logic

--- src/drivers/password_handler.py
+++ src/drivers/password_handler.py
@@ -7,8 +7,8 @@ class PasswordHandler:
         # que senhas iguais resultem em hashes diferentes, melhorando a segurança.
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
         # faz o hash com o salt
-        return hashed_password #a idea é não ser legível
+        return hashed_password.decode('utf-8') #a idea é não ser legível
         #checagem das senhas encriptadas
     
     def check_password(self, password: str, hashed_password: str) -> bool:
-        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
